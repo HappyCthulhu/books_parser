8ec68ee2143a3fd219e9620f16200ac80201019c
HappyCthulhu <valeriyg98@gmail.com>
2020-12-29 18:47:53 +0300

    some changes

--- py_files/locators.py
+++ py_files/locators.py
@@ -18,17 +18,20 @@ class LabirintLocators:
     editor_xpath = '//a[@data-event-label="editor"]/text()'
     illustrator_xpath = '//*[text()="Художник: "]/text()/following-sibling::a/text()'
     genres_xpath = '//div[@id="thermometer-books"]//a/span/text()'
+    product_cover = '//a[@class="product-title-link"]/@href'
 
 
 class BooksLocators:
-    year_xpath = '//table[@class="specifications_table"]//tr[2]/td[2]/text()'
+    year_xpath = '//td[text()="Дата выхода:"]/following::td[1]/text()'
+    # year_xpath = '//table[@class="specifications_table"]//tr[2]/td[2]/text()'
     name_xpath = '//h1/text()'
     no_price_xpath = ''
-    price_without_discount_xpath = '//h3[@class="h3 book-price sale"]/text()'
-    old_price_xpath = '//p[@class="p book-price-full-sale"]/text()'
-    new_price_xpath = '//h3[@class="h3 book-price sale"]/text()[1]'
+    # price_without_discount_xpath = '//h3[@class="h3 book-price sale"]/text()'
+    # old_price_from_labirint_xpath = '//p[@class="p book-price-full-sale"]/text()'
+    # new_price_xpath = '//h3[@class="h3 book-price sale"]/text()[1]'
     weight_xpath = '//td[text()="Масса:"]/following-sibling::td/text()'
-    genre_xpath = '//div[@class="route-wrap"]/span[last()]/a/span/text()'  # нужно взять последний элемент!
+    # genres_xpath = '//div[@class="route-wrap"]/span/a/span/text()'  # все жанры берет
+    genres_xpath = '//div[@class="route-wrap"]/span[3]/a/span/text()'  # третий элемент
     # text_dimensions_xpath = '//tbody/tr[7]/td[1]/text()'
     dimensions_xpath = '//tbody/tr[7]/td[2]/text()'
     author_xpath = '//div[@class="author-link-wrap"]//text()'
@@ -44,4 +47,11 @@ class BooksLocators:
 class OzonLocators:
     exist_text = '//div[@class="b6r7"]/strong/text()'
     product_page_link = '//div[@class="container b6e3"]//a[2]'
+    cheapest_book_page_link = '//span[text()="Лучшая цена на Ozon"]/following-sibling::a'
     photo_link = '//div[@class="a8n3"]/div/img'
+    # found_items = '//div[@class="ui-a1j5 ui-a1h"]' # хуевая проверка, срабатывает тут: https://www.ozon.ru/search/?text=5-18-000702-%D0%A5&from_global=true&brand=87317976
+    # found_items_1 = '//div[contains(text(), "По запросу")]' # проверка наличия книги видимо
+    isbn_exist = '//div[@class="b6r7"]/strong/text()' # проверка наличия книги видимо
+
+    price = '//div[@class="client-state"]/div[@id="state-addToFavorite-347772-lfskdjault-1"]/@data-state'
+    # price_ozon_premium = '//div[@class="c8x9"]/span[@class="b0r3"]/span[contains(text(), "Цена с")]'
